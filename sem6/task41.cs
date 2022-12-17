2d8e3d526231f64a59bce89904e26303e47fe0ea
mercysider <mercysider@mail.ru>
2022-12-17 12:10:13 +0300

    sem6 is ready

---
+++ sem6/task41.cs
@@ -0,0 +1,16 @@
+﻿int EvenInUserInput(int qty)  
+{
+    int count = 0;
+
+    for (int i = 0; i < qty; i++)
+    {
+        Console.Write($"Введите число {i+1}: ");
+        if (int.Parse(Console.ReadLine()!) > 0) count++;
+    }
+    return count;    
+}  
+    
+Console.Write("Сколько чисел вы хотите ввести? ");
+int intSize = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Положительных чисел: {EvenInUserInput(intSize)}");
---
+++ sem6/task43.cs
@@ -0,0 +1,40 @@
+﻿void FindCrossPointFor2Lines(double k1, double b1, double k2, double b2)  
+{
+    double x = 0;
+    double y = 0;
+
+    string outb1 = "";
+    string outb2 = "";
+
+    if (b1 < 0) outb1 = $"{b1}";
+    else if (b1 > 0) outb1 = $"+{b1}";
+
+    if (b2 < 0) outb2 = $"{b2}";
+    else if (b2 > 0) outb2 = $"+{b2}";
+
+    if (k1 == k2) 
+    {
+        if (b1 == b2) Console.WriteLine($"Прямые y={k1}x{outb1} и y={k2}x{outb2} совпадают!");
+        else Console.WriteLine($"Прямые y={k1}x{outb1} и y={k2}x{outb2} параллельны!");
+    } 
+    else 
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k1 * x + b1;
+        x = Math.Round(x, 2);
+        y = Math.Round(y, 2);
+
+        Console.WriteLine($"Точка пересечения для прямых y={k1}x{outb1} и y={k2}x{outb2}: ({x}, {y})"); 
+    }      
+}  
+    
+Console.Write("Введите значение k1 для уравнения y=k1*x+b1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write($"Введите значение b1 для уравнения y={k1}*x+b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k2 для уравнения y=k2*x+b2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+Console.Write($"Введите значение b2 для уравнения y={k2}*x+b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+
+FindCrossPointFor2Lines(k1, b1, k2, b2);
